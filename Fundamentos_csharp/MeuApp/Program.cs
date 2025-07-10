a84c8c8bb920f4107a2bc7439aa354f30f645ac6
GabrielInacio03 <inaciogs03@gmail.com>
2025-07-10 13:24:30 -0300

    primeiro commit

---
+++ Fundamentos_csharp/MeuApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando!");
+            Console.WriteLine("============");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("============");
+        }
+    }
+}
