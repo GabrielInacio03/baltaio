ac7e0fcd8a185432437ce64371ea169be761a761
GabrielInacio03 <inaciogs03@gmail.com>
2025-07-11 00:58:57 -0300

    removendo arquivos

--- Fundamentos_csharp/MeuApp/Program.cs
+++ Fundamentos_csharp/MeuApp/Program.cs
@@ -6,7 +6,9 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine();
             System.Console.WriteLine("testando variáveis");
+            System.Console.WriteLine();
            // int idade; // correto inicia com ZERO
             // int idade2 = 25;  //correto
             // var idade3 = 25; //correto
