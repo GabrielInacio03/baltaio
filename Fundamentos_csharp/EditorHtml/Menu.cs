60b3d76f37a3b485ba6ca2f3bb1c5e6bf9852c78
GabrielInacio03 <inaciogs03@gmail.com>
2025-07-12 19:06:39 -0300

    Mão Na Massa: EditorHTML

---
+++ Fundamentos_csharp/EditorHtml/Menu.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            DrawScreen();
+            WriteOptions();
+            string opcao = Console.ReadLine();
+        }
+        public static void DrawScreen()
+        {
+            DrawScreenHeader();
+            DrawScreenSection();
+            DrawScreenFooter();
+        }
+        public static void DrawScreenHeader()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+        }
+        public static void DrawScreenSection()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+        }
+        public static void DrawScreenFooter()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("_> Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - SAIR");
+            Console.SetCursorPosition(3, 10);
+            
+        }
+
+    }
+}
