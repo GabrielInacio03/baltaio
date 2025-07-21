343788db3669291b4173f38ab9854fdd1649993d
GabrielInacio03 <inaciogs03@gmail.com>
2025-07-21 11:46:15 -0300

    iniciando orientação a objetos

---
+++ Fundamentos_POO/Payments/Models/Customer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payments.Models
+{
+    public class Customer
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ Fundamentos_POO/Payments/Models/Pagamento.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payments.Models
+{
+    public class Pagamento
+    {
+        public DateTime DataPagamento { get; set; }
+
+        public void Pagar(){}
+    }
+}
